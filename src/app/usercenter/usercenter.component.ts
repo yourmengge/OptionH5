c21510ebc69ed547c0984cf822ded8fd58645979
张萌立 <zhangmengli57@gmail.com>
2019-04-29 08:48:20 +0800

    成交市值与成交价格交换，世纪方略判断修改

--- src/app/usercenter/usercenter.component.ts
+++ src/app/usercenter/usercenter.component.ts
@@ -69,7 +69,7 @@ export class UsercenterComponent implements OnInit, OnDestroy {
   }
 
   goto2(url) {
-    if (url === 'withdraw' || (url === 'recharge' && this.data.logo === 'eastnsd')) {
+    if (url === 'withdraw' || (url === 'recharge' && this.data.logo === 'dfqq')) {
       this.http.getCard().subscribe(res => {
         if (this.data.isNull(res)) {
           this.data.ErrorMsg('请先绑定银行卡');
